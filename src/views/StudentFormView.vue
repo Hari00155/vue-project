d88eacd9d6f0f17fc20fd03dc4e4a61ef30404bf
Hari00155 <ahariharananandan@gmail.com>
2025-05-16 16:42:29 +0530

    update staffs form

--- src/views/StudentFormView.vue
+++ src/views/StudentFormView.vue
@@ -1,52 +1,75 @@
 <template>
   <Container>
-   <h1> Student Form </h1>
+   <h1> Staffs Form </h1>
 
-   <div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon1">@</span>
-  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-</div>
-
-<div class="input-group mb-3">
-  <input type="text" class="form-control" placeholder="Recipient’s username" aria-label="Recipient’s username" aria-describedby="basic-addon2">
-  <span class="input-group-text" id="basic-addon2">@example.com</span>
-</div>
-
-<div class="mb-3">
-  <label for="basic-url" class="form-label">Your vanity URL</label>
-  <div class="input-group">
-    <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
-    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4">
+   <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputFirstName" class="form-label">First Name</label>
+    <input type="First Name" class="form-control" id="inputFirstName">
   </div>
-  <div class="form-text" id="basic-addon4">Example help text goes outside the input group.</div>
+  <div class="col-md-6">
+    <label for="inputLastName" class="form-label">Last Name</label>
+    <input type="LastName" class="form-control" id="inputLastName">
+  </div>
+ <div class="row mb-3">
+  <label for="colFormLabel" class="col-sm-2 col-form-label">Email</label>
+  <div class="col-sm-25">
+    <input type="email" class="form-control" id="colFormLabel" placeholder="XYZ@Email.com">
+  </div>
 </div>
-
-<div class="input-group mb-3">
-  <span class="input-group-text">$</span>
-  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-  <span class="input-group-text">.00</span>
+ <div class="col-md-6">
+    <label for="DOB" class="form-label">Dob</label>
+    <input type="text" class="form-control" id="inputDOB">
+  </div>
+  
+  <div class="col-md-4">
+    <label for="Bloodgroup" class="form-label">Blood Group</label>
+    <select id="inputBloodgroup" class="form-select">
+      <option selected>Choose...</option>
+      <option>A+</option>
+      <option>A-</option>
+      <option>AB+</option>
+      <option>AB-</option>
+      <option>B+</option>
+      <option>B-</option>
+      <option>O+</option>
+      <option>O-</option>
+    </select>
+  </div>
+  <div class="col-md-2">
+    <label for="inputAGE" class="form-label">Age</label>
+    <input type="text" class="form-control" id="inputAGE">
+  </div>
+  <div class="form-floating">
+  <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+  </select>
+  <label for="floatingSelect">Year of Experience</label>
 </div>
-
-<div class="input-group mb-3">
-  <input type="text" class="form-control" placeholder="Username" aria-label="Username">
-  <span class="input-group-text">@</span>
-  <input type="text" class="form-control" placeholder="Server" aria-label="Server">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">With textarea</span>
-  <textarea class="form-control" aria-label="With textarea"></textarea>
-</div>
-
-
-<select class="form-select" aria-label="Default select example">
-  <option selected>Open this select menu</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-</select>
-
-
+<div class="row mb-3">
+    <label for="inputPhoneNO" class="col-sm-2 col-form-label">Phone No</label>
+    <div class="col-sm-25">
+      <input type="email" class="form-control" id="inputPhoneNo">
+    </div>
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+   <div class="col-12">
+    <button class="btn btn-primary" type="submit">Submit form</button>
+  </div>
+ 
+</form>
+  
   </Container>
 </template>
 
