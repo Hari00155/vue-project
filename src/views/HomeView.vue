65d04b838a4863babff111effc67b4fb58a6092c
Hari00155 <ahariharananandan@gmail.com>
2025-07-23 20:26:45 +0530

    update a bg image and footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,18 @@
 <template>
-  Hello World
+<div class="card text-bg-dark position-relative">
+  <img src="https://www.univariety.com/blog/wp-content/uploads/2020/08/Blog-banner-image-1.jpg" class="card-img" alt="...">
+
+  <!-- Top Center Black Text -->
+  <div class="position-absolute top-0 start-50 translate-middle-x mt-3">
+    <h2 class="text-black fw-bold">DEVELOPER UNIVERSITY</h2>
+  </div>
+
+
+
+    
+
+
+
+  
+</div>
 </template>
